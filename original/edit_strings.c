d70ffa6184e576bcefc0c77138dd1c6ebf51a4df
Deaconlakes <alabiolalekan6@gmail.com>
2023-09-18 08:55:55 -0700

    bettied

--- original/edit_strings.c
+++ original/edit_strings.c
@@ -1,67 +1,63 @@
 #include "shell.h"
-
 /**
- * _strcat - copy string to another string.
- * @dest: char
- * @src: char
+ * _strcat - function that copy string to another string.
+ * @dest: char input
+ * @src: char src
  * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
-    int d = 0;
-    int s = 0;
+	int d = 0;
+	int s = 0;
 
-    for (d = 0; dest[d] != '\0'; d++)
-    {
-    }
+	for (d = 0; dest[d] != '\0'; d++)
+	{
+	}
 
-    for (s = 0; src[s] != '\0'; s++, d++)
-    {
-        dest[d] = src[s];
-    }
+	for (s = 0; src[s] != '\0'; s++, d++)
+	{
+		dest[d] = src[s];
+	}
 
-    dest[d] = '\0';
-    return (dest);
+	dest[d] = '\0';
+		return (dest);
 }
-
 /**
- * _strcmp - compare the values of a string
- * @s1: character
- * @s2: character
+ * _strcmp - funtion that compare the values of a string
+ * @s1: string pointer
+ * @s2: string pointer
  * Return: 0
  */
 int _strcmp(char *s1, char *s2)
 {
-    int a;
+	int a;
 
-    int len_s1 = _strlen(s1);
-    int len_s2 = _strlen(s2);
+	int len_s1 = _strlen(s1);
+	int len_s2 = _strlen(s2);
 
-    for (a = 0; a < len_s1 && a < len_s2; a++)
-    {
-        if (s1[a] != s2[a])
-            return ((int)s1[a] - s2[a]);
-    }
-    return (0);
+	for (a = 0; a < len_s1 && a < len_s2; a++)
+	{
+		if (s1[a] != s2[a])
+			return ((int)s1[a] - s2[a]);
+	}
+	return (0);
 }
-
 /**
- * _strlen - copies the string pointed to by src into dest
- * @s: A pointer
+ * _strlen - funtion copies the string pointed to by src into dest
+ * @s: pointer character
  * Return: char pointer to dest
  */
 int _strlen(char *s)
 {
-    int ch = 0;
+	int ch = 0;
 
-    while (*(s + ch) != '\0')
-    {
-        ch++;
-    }
+	while (*(s + ch) != '\0')
+	{
+		ch++;
+	}
 
-    return (ch);
+	return (ch);
 }
-
 /**
  * _strncmp -  function that compares two strings.
  * @s1: string one
@@ -71,37 +67,35 @@ int _strlen(char *s)
  */
 size_t _strncmp(char *s1, char *s2, size_t n)
 {
-    size_t i, j;
+	size_t i, j;
 
-    for (j = 0; s1[j] != '\0' && j < n; j++)
-    {
-        i = s1[j] - s2[j];
+	for (j = 0; s1[j] != '\0' && j < n; j++)
+	{
+		i = s1[j] - s2[j];
 
-        if (i != 0)
-        {
-            return (i);
-        }
-    }
-    return (0);
+		if (i != 0)
+		{
+			return (i);
+		}
+	}
+	return (0);
 }
 
 /**
- * _strcpy - copies the string pointed to by src into dest
+ * _strcpy - function that  copies the string pointed to by src
  * @dest: destination of the copy
- * @src: source of the copy
+ * @src: pointer to souce to be copy
  * Return: char pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i = 0;
+	int i = 0;
 
-    while (*(src + i) != '\0')
-    {
-        *(dest + i) = *(src + i);
-        ++i;
-    }
-    *(dest + i) = *(src + i);
-
-    return (dest);
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		++i;
+	}
+	*(dest + i) = *(src + i);
+	return (dest);
 }
-
--- original/edit_tokenizer.c
+++ original/edit_tokenizer.c
@@ -1,77 +1,68 @@
 #include "shell.h"
-
 /**
- * _strtok - breaks the string s1 into tokens and null-terminates them.
- * Delimiter-Characters at the beginning and end
- *of str are skipped. On each subsequent call delim may change.
- * @str: string to tokenize
- * @delim: string with the character that delimit srt.
- * Return: the first/next token if possible, a null-pointer otherwise.
+ * _strtok - function that breaks the string into tokens
+ * @str: string to be tokenize
+ * @del: delimit strings char
+ * Return: first/next token, otherwise null-pointer
  **/
 char *_strtok(char *str, const char *del)
 {
-        static char *p;
+	static char *p;
 
-        if (str)
-                p = str;
-        else if (!p)
-                return (0);
-        str = p + _strspn(p, del);
-        p = str + _strcspn(str, del);
-        if (p == str)
-                return (p = 0);
-        p = *p ? *p = 0, p + 1 : 0;
-        return (str);
+	if (str)
+		p = str;
+	else if (!p)
+		return (0);
+	str = p + _strspn(p, del);
+	p = str + _strcspn(str, del);
+	if (p == str)
+		return (p = 0);
+	p = *p ? *p = 0, p + 1 : 0;
+	return (str);
 }
 /**
- * _strcspn - computes the length of the maximum initial segment of the string
- * pointed to by s1which consists entirely of characters not from the
- * string pointed to by s2.
+ * _strcspn - function that computes the length of the string
  * @s1: string to check
- * @s2: string useful to compare
- * Return: the length of the segment.
+ * @s2: string to compare
+ * Return: length of the segment.
  **/
 size_t _strcspn(const char *s1, const char *s2)
 {
-        size_t ret = 0;
+	size_t ret = 0;
 
-        while (*s1)
-        {
-                if (_strchr(s2, *s1))
-                        return (ret);
-                s1++, ret++;
-        }
-        return (ret);
+	while (*s1)
+	{
+		if (_strchr(s2, *s1))
+			return (ret);
+		s1++, ret++;
+	}
+	return (ret);
 }
 /**
- * _strspn - computes the length of the maximum initial segment of the string
- * pointed to by s1 which consists entirely of characters from the string
- * pointed to by s2.
+ * _strspn - function that computes the length of the string
  * @s1: strint to compute the lengh
  * @s2: string delimit
  * Return: the length of the segment.
  **/
 size_t _strspn(const char *s1, const char *s2)
 {
-        size_t ret = 0;
+	size_t ret = 0;
 
-        while (*s1 && _strchr(s2, *s1++))
-                ret++;
-        return (ret);
+	while (*s1 && _strchr(s2, *s1++))
+		ret++;
+	return (ret);
 }
 /**
- * _strchr - locates the ﬁrst occurrence of c (converted to a char) in the
- * string pointed to by s. The terminating null character is considered to be
- * part of the string.
+ * _strchr - function that locates the ﬁrst occurrence of char in the
+ * string pointer
  * @s: string
- * @c: character
- * Return: a pointer to the located character, or a null pointer
- * if the character does not occur in the string.
+ * @ch: character
+ * Return: a pointer, otherwise a null pointer
  **/
 char *_strchr(const char *s, int ch)
 {
-        while (*s != (char)ch)
-                if (!*s++)
-                        return (0);
-        return ((char *)s);
+	while (*s != (char)ch)
+		if (!*s++)
+			return (0);
+	return ((char *)s);
 }
