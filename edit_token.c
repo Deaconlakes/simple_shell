b1b242a95ca3192d1c0fdb26d23e2e115dcb1389
Donchess1 <stepabod@yahoo.com>
2023-09-20 19:23:11 +0000

    added

--- edit_token.c
+++ edit_token.c
@@ -11,6 +11,7 @@ char **_get_token(char *input)
 	char *token = NULL;
 	size_t i = 0;
 	int size = 0;
+	char *del = " \n\t\r;";
 
 	if (input == NULL)
 		return (NULL);
@@ -27,12 +28,12 @@ char **_get_token(char *input)
 	if (user_command == NULL)
 		return (NULL);
 
-	token = _strtok(input, " \n\t\r");
+	token = _strtok(input, del);
 	i = 0;
 	while (token != NULL)
 	{
 		user_command[i] = token;
-		token = _strtok(NULL, " \n\t\r");
+		token = _strtok(NULL, del);
 		i++;
 	}
 	user_command[i] = NULL;
