1ac9a0694137aa708b99a561082d56dc80462588
Donchess1 <stepabod@yahoo.com>
2023-09-20 09:25:05 +0000

    set

--- path.c
+++ path.c
@@ -1,26 +1,30 @@
-#include "newheader.h"
+#include "shell.h"
+/**
+ * pathcheck - matches the tokenized input with a variable PATH.
+ * @env: enviromente local
+ * Return: value of PATH.
+ */
 
-int path()
+char *pathcheck(char **env)
 {
-	char *path;
-	char *path_copy;
-	char *dir;
-	path = getenv("PATH");
+	size_t first = 0, var = 0, counter = 5;
+	char *path = NULL;
 
-    if (path == NULL) {
-        return 0;
-    }
+	for (first = 0; strncmp(env[first], "PATH=", 5); first++)
+		;
+	if (env[first] == NULL)
+		return (NULL);
 
-    path_copy = strdup(path);
-    if (path_copy == NULL) {
-        return 0;  
-    }
+	for (counter = 5; env[first][var]; var++, counter++)
+		;
+	path = malloc(sizeof(char) * (counter + 1));
 
-    dir = strtok(path_copy, ":");
-    while (dir != NULL) {
-        dir = strtok(NULL, ":");
-    }
+	if (path == NULL)
+		return (NULL);
 
-    free(path_copy);
-    return 0; 
+	for (var = 5, counter = 0; env[first][var]; var++, counter++)
+		path[counter] = env[first][var];
+
+	path[counter] = '\0';
+	return (path);
 }
